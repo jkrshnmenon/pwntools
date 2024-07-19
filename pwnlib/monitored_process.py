58260a8a4c222f7bcc18de3023c9b6b0c53d0d60
gor <gor@kali>
2024-07-19 11:38:59 -0700

    finished work

--- pwnlib/monitored_process.py
+++ pwnlib/monitored_process.py
@@ -4,12 +4,25 @@ from __future__ import absolute_import
 from pwnlib import gdb
 from pwnlib import tubes
 from pwnlib.log import Logger
+from pwnlib.elf import ELF
 
 import logging
 
 __all__ = ['monitored_process']
 
 class monitored_process(Logger):
+    output_functions = [
+        'printf', 'fprintf', 'vprintf', 'vfprintf',
+        'obprintf', 'opprintf', 'fputs', 'fputc', 'fwrite', 'fputs_unlocked', 'fputc_unlocked', 'putw', 'putws',
+        'puts', 'putchar', 'putc', 'fsync', 'fdatasync',
+        'dprintf', 'vdprintf', 'write', 'pwrite', 'writev', 'pwritev',
+        'wprintf', 'fwprintf', 'swprintf', 'vwprintf', 'vfwprintf', 'vswprintf',
+        'asprintf', 'vasprintf', 'send', 'sendto', 'sendmsg',
+        'perror',
+        'write', 'pwrite', 'writev', 'pwritev', 'send', 'sendto', 'sendmsg',
+        'putc_unlocked', 'putw', 'putwc', 'putws'
+    ]
+
     def __init__(self, pattern: bytes, address: int, *args, **kwargs):
         """Monitors a process for a pattern in the communication stream and breakpoints at an address when this pattern is detected.
 
@@ -25,7 +38,21 @@ class monitored_process(Logger):
         self.setLevel(logging.DEBUG)
         
         # Attach GDB to the process
-        self.gdb_pid,self.gdb = gdb.attach(self.proc, exe=self.proc.argv[0], gdbscript="continue", api=True)
+        self.gdb_pid, self.gdb = gdb.attach(self.proc, exe=self.proc.argv[0], gdbscript="continue", api=True)
+        
+        # Get the ELF object for the process
+        self.elf = ELF(self.proc.argv[0])
+        self.debug(f"ELF object created for: {self.proc.argv[0]}")
+        
+        # Set breakpoints at output functions in the PLT
+        self.set_output_function_breakpoints()
+    
+    def set_output_function_breakpoints(self):
+        for func in self.output_functions:
+            if func in self.elf.plt:
+                plt_address = self.elf.plt[func]
+                self.debug(f"Setting breakpoint at {func} in PLT at {hex(plt_address)}")
+                self.gdb.execute(f'break *{hex(plt_address)}')
     
     def sendline(self, data: bytes):
         """Send a line to the process.
@@ -50,8 +77,8 @@ class monitored_process(Logger):
         # Monitor for pattern
         if self.pattern in data:
             self.debug(f"Pattern detected: {self.pattern}")
-            command = 'break *0x%x' %self.address 
+            command = 'break *0x%x' % self.address
             self.debug(command)
-            self.gdb.execute('break *0x%x' %self.address )
+            self.gdb.execute(command)
         
         return data
